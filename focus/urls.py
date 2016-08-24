2ccd4aafec15493678bdaef46044036fdd4b782d
Rolight <flsnnx@gmail.com>
2016-08-24 14:58:49 +0800

    views part 4 finished.

--- focus/urls.py
+++ focus/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import include, url
 from . import views
 
+app_name = 'focus'
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^register/$', views.register, name='register'),
--- focus/views.py
+++ focus/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.core.exceptions import ObjectDoesNotExist
+from django.urls import reverse
+
 
 from .models import Article, Comment, Poll, NewUser
-from .forms import LoginForm
+from .forms import LoginForm, CommentForm
+
 
 
 # Create your views here.
@@ -58,3 +61,75 @@ def log_out(request):
     url = request.POST.get('source_url', '/focus/')
     logout(request)
     return redirect(url)
+
+def article(request, article_id):
+    article=  get_object_or_404(Article, id=article_id)
+    content = markdown2.markdown(article.content, extras=[
+        "code-friendly", "fenced-code-blocks", "header-ids",
+        "toc", "metadata"
+    ])
+    commentform = CommentForm()
+    loginform = LoginForm()
+    comments = article.comment_set.all()
+
+    return render(
+        request,
+        'article_page.html',
+        {
+            'article': article,
+            'loginform': loginform,
+            'commentform': commentform,
+            'content': 'content',
+            'comments': comments
+        }
+    )
+
+@login_required
+def comment(request, article_id):
+    form = CommentForm(request.POST)
+    url = reverse('focus:article', args=(article_id,))
+    if form.is_valid():
+        user = request.user
+        article = Article.objects.get(id=article_id)
+        new_comment = form.cleaned_data['comment']
+        c = Comment(content=new_comment, article_id=article_id)
+        c.user = user
+        c.save()
+        article.comment_num += 1
+    return redirect(url)
+
+@login_required
+def get_keep(request, article_id):
+    logged_user = request.user
+    article = Article.objects.get(id=article_id)
+    articles = logged_user.article_set.all()
+    if article not in articles:
+        article.user.add(logged_user)
+        article.keep_num += 1
+        article.save()
+
+        return redirect(reverse('focus:index'))
+    else:
+        url = reverse('focus:article', args=(article_id))
+        return redirect(url)
+
+@login_required
+def get_keep(request, article_id):
+    logged_user = request.user
+    article = Article.objects.get(id=article_id)
+    polls = logged_user.poll_set.all()
+    articles = []
+    for poll in polls:
+        articles.append(poll.article)
+
+    if article in articles:
+        url = reverse('focus:article', args=(article_id,))
+        return redirect(url)
+    else:
+        article.poll_num += 1
+        article.save()
+        poll = Poll(user=logged_user, article=article)
+        poll.save()
+        return redirect(reverse('focus:index'))
+
+
